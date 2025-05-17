84d724a93fc2e0763149ebcb9e103c42d9ec8b3a
arina-chumachenko <92745624+arina-chumachenko@users.noreply.github.com>
2025-05-17 15:03:57 +0300

    Update eval.py

--- scripts/CoRe/eval/eval.py
+++ scripts/CoRe/eval/eval.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 from IPython import get_ipython
 
 ipython = get_ipython()
@@ -12,17 +12,17 @@ if ipython is not None:
 import warnings
 warnings.filterwarnings("ignore")
 
-# import regex
+import regex
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
-# import random
+import random
 import numpy as np
-# from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import FormatStrFormatter
 import torch
 import torch.backends.cuda
 import matplotlib_inline
-# import matplotlib.pyplot as plt
-# import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 matplotlib_inline.backend_inline.set_matplotlib_formats('pdf', 'svg')
 
@@ -34,7 +34,7 @@ from nb_utils.clip_eval import aggregate_similarities
 from nb_utils.cache import DistributedCache, Cache
 from nb_utils.experiments_viewer import ExpsViewer
 from nb_utils.clip_eval import ExpEvaluator
-# from nb_utils.images_viewer import MultifolderViewer
+from nb_utils.images_viewer import MultifolderViewer
 from nb_utils.configs import live_object_data
 
 def make_dict(path):
@@ -85,13 +85,21 @@ device = torch.device('cuda', 0) if torch.cuda.is_available() else torch.device(
 
 evaluator = ExpEvaluator(device)
 
-dir_path = "/home/jovyan/shares/SR006.nfs2/aschumachenko/aschumachenko"
+dir_path = ""
 base_path = f"{dir_path}/core/res_CR/"
 eval_path = f'{base_path}*/eval.cache'
-concepts = ['bear_plushie', 'colorful_sneaker'] # 'dog6', 'cat', 'teapot', 'grey_sloth_plushie', 'shiny_sneaker'] # ['dog6', 'cat2', 'duck_toy', 'teapot', 'cat']    #  'dog6', 'cat2', 'duck_toy', 'teapot', 'dog6', 'clock', 'cat', 'backpack', 'cat2', 'berry_bowl', 'red_cartoon', 'poop_emoji', 'dog', 'monster_toy', 'grey_sloth_plushie', 'vase', 'dog5'
-method_name = 'CR_sdxl'  # 'CR'/'CD'
+concepts = [
+    'dog6', 
+    'cat', 
+    'teapot', 
+    'grey_sloth_plushie', 
+    'bear_plushie',
+    'shiny_sneaker', 
+    'colorful_sneaker',
+]
+method_name = 'CR_sdxl'
 chp = 300
-exps_nums = range(0, 7) # [0] # range(17, 23) # [25, 26] # range(10, 18)
+exps_nums = range(0, 7)
 
 exps = [f'00{i}-res-{concept}_{method_name}' for i in exps_nums for concept in concepts]
 # [a for a in os.listdir(base_path)]
@@ -123,25 +131,7 @@ cache_data = ti_evaluate_cache.get()
 
 # Ensure aggregate_similarities can handle None values appropriately
 d = {k: {**v, **aggregate_similarities(v)} for k, v in cache_data.items()}
-# print(d[('0015-res-cat_CR', ('1000', '50', '7.0'))].keys())
 # d[key]
 
 res = make_dict(eval_path)
 print(res)
-
-# запустив код, можно посмотреть сохраненные картинки, если они были правильно сохранены
-# ti_exps_viewer = ExpsViewer(
-#     # Base path to experiments
-#     f'{dir_path}/diffusers/examples/core/res_CR',
-#     # Filter experiments
-#     # exp_filter_fn=lambda x: x in [x for x in os.listdir('results') if "CD" not in x],
-#     # Filter prompts
-#     # filter_fn='a photo of a sks',
-#     # Defines number of columns in experiment selector
-#     ncolumns=4,
-#     # Whether to load all images only on demand
-#     lazy_load=True,
-#     # ExpEvaluator instance to perform evaluation. See scripts/*/inference.sh
-#     evaluator=None,
-# )
-# ti_exps_viewer.view()
